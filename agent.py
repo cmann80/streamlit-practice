a75edf20dcf17f484117ebdacae0b0931b99700d
Chris Mann <chrispmann@TP-Dorayaki>
2023-05-10 18:42:17 -0700

    added state.py

--- agent.py
+++ agent.py
@@ -1,27 +1,32 @@
 
 
 from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
 from langchain.agents import load_tools
 from langchain.agents import initialize_agent
 from langchain.agents import AgentType
+from langchain.chat_models import ChatOpenAI
 from langchain import ConversationChain
 
+chat = ChatOpenAI(temperature=0)
 
 llm = OpenAI(temperature=0)
 
-# tools = load_tools(["serpapi", "llm-math"], llm = llm)
+tools = load_tools(["serpapi", "llm-math"], llm = llm)
 
 # agent = initialize_agent(tools, llm,
 #                          agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose = True)
 
 # agent.run("what was the high temprature in Seattle today? What is that number raised to the .023 power?")
 
-conversation = ConversationChain(llm=llm, verbose=True)
+# conversation = ConversationChain(llm=llm, verbose=True)
 
-output = conversation.predict(input = "Hi there!")
-print(output)
+# output = conversation.predict(input = "Hi there!")
+# print(output)
 
-output = conversation.predict(input="I'm doing well! Just having a conversation with an AI.")
-print(output)
+# output = conversation.predict(input="I'm doing well! Just having a conversation with an AI.")
+# print(output)
+
+agent = initialize_agent(tools, chat,
+                         agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION, verbose = True)
+
+print(agent.run("Who is Olivia Wilde's boyfriend? What is his current age raised to the 0.23 power?"))
