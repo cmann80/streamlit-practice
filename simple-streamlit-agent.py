d20d8fde0251a1cf90260f1981fe9337ced5a401
Chris Mann <chrispmann@gmail.com>
2023-06-01 15:23:35 -0700

    minor formatting changes

--- simple-streamlit-agent.py
+++ simple-streamlit-agent.py
@@ -16,8 +16,7 @@ llm = OpenAI(temperature=0)
 
 tools = load_tools(["serpapi", "llm-math"], llm = llm)
 
-agent = initialize_agent(tools, chat,
-                         agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION, verbose = True)
+agent = initialize_agent(tools, chat, agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION, verbose = True)
 
 text = st.text_area("Enter Text")
 submit = st.button("Generate Response")
